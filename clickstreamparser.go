c57b415274a6fbe7f7599fae769249a4c4edcc7b
gevgev <gevgev@yahoo.com>
2016-04-07 19:58:52 -0700

    Added files via upload
    Initial upload

---
+++ clickstreamparser.go
@@ -0,0 +1,259 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	test_KEY_A = "4144287C7000AB5ADBF2B0E50A"
+	test_KEY_B = "424427ABE800F70B0C4D6F746F722053706F727473030B4D6F746F7273706F7274733164646464643164306464306430303164646464646464646464303030303030303130303030303030303030303030303030608C0A"
+	test_KEY_C = "43442878E201F82B5742AE4741000713AF3B0A"
+	test_KEY_D = "44287C448C4D04DA04D60000000000DDA20A"
+	test_KEY_E = "45442852144444535800585E0F44277150FF7536E8B2C3EF58585300000064680A"
+	//test_KEY_F = "46?"
+	test_KEY_G = "4744287C560C5050503A504149442D504944D8450A"
+	//						^^
+	test_KEY_H  = "4844287C6B47486A7926D244286060FAD50A"
+	test_KEY_H1 = "4844287C4C4D04DA04D60000000000DDA20A"
+	test_KEY_H2 = "4844287C7C51000003A20000000000F36B0A"
+	test_KEY_H3 = "4844287C7044AB5ADF000000000000AE8A0A"
+	test_KEY_H4 = "48442878D74F007200000000000000A3990A"
+	test_KEY_H5 = "4844287C554234E74E417A0000000069AC0A"
+	test_KEY_H6 = "4844287C5D4100000100000000000077BA0A"
+
+	test_KEY_I = "4944287C545600EBE822D55B0A"
+	//test_KEY_J = "4A?"
+	//test_KEY_K = array of samples below
+	//test_KEY_L = "4C?"
+	test_KEY_M  = "4D4428629E4500140161EBA10A"
+	test_KEY_P  = "50442877A600000002250A"
+	test_KEY_S  = "5344287C58F8E211EF930A"
+	test_KEY_U  = "55442877A600059CAA293233322E343400020000000000000000000000000000000000000000000000007F0A"
+	test_KEY_U1 = "55442841ED000038FD833233322E343400020000000000000000000000000000000000000000000000002A0A"
+	test_KEY_U2 = "554428839D00008B8D723233322E34340002000000000000000000000000000000000000000000000000660A"
+	test_KEY_V  = "5644287C5600000000EBE8220656FFFFD7460A"
+)
+
+var test_KEY_K = [...]string{"4B44287C4D00DE6D0A",
+	"4B44287C4D01DE6D0A",
+	"4B44287C4D02DE6D0A",
+	"4B44287C4D0ADE6D0A",
+	"4B44287C4D0BDE6D0A",
+	"4B44287C4D0CDE6D0A",
+	"4B44287C4D0DDE6D0A",
+	"4B44287C4D0EDE6D0A",
+	"4B44287C4D0FDE6D0A",
+	"4B44287C4D10DE6D0A",
+	"4B44287C4D11DE6D0A",
+	"4B44287C4D12DE6D0A",
+	"4B44287C4D13DE6D0A",
+	"4B44287C4D14DE6D0A",
+	"4B44287C4D15DE6D0A",
+	"4B44287C4D16DE6D0A",
+	"4B44287C4D17DE6D0A",
+	"4B44287C4D18DE6D0A",
+	"4B44287C4D19DE6D0A",
+	"4B44287C4D1ADE6D0A",
+	"4B44287C4D1BDE6D0A",
+	"4B44287C4D1CDE6D0A",
+	"4B44287C4D1DDE6D0A",
+	"4B44287C4D1EDE6D0A",
+	"4B44287C4D1FDE6D0A",
+	"4B44287C4D20DE6D0A",
+	"4B44287C4D21DE6D0A",
+	"4B44287C4D22DE6D0A",
+	"4B44287C4D23DE6D0A",
+	"4B44287C4D24DE6D0A",
+	"4B44287C4D25DE6D0A",
+	"4B44287C4D26DE6D0A",
+	"4B44287C4D27DE6D0A",
+	"4B44287C4D28DE6D0A",
+	"4B44287C4D29DE6D0A",
+	"4B44287C4D2ADE6D0A",
+	"4B44287C4D2BDE6D0A",
+	"4B44287C4D2CDE6D0A",
+	"4B44287C4D2DDE6D0A",
+	"4B44287C4D2EDE6D0A",
+	"4B44287C4D2FDE6D0A",
+	"4B44287C4D30DE6D0A",
+	"4B44287C4D31DE6D0A",
+	"4B44287C4D32DE6D0A",
+	"4B44287C4D33DE6D0A",
+	"4B44287C4D34DE6D0A",
+	"4B44287C4D35DE6D0A",
+	"4B44287C4D36DE6D0A",
+	"4B44287C4D37DE6D0A",
+	"4B44287C4D38DE6D0A",
+	"4B44287C4D39DE6D0A",
+	"4B44287C4D3ADE6D0A",
+	"4B44287C4D3BDE6D0A",
+	"4B44287C4D3CDE6D0A",
+	"4B44287C4D3DDE6D0A",
+	"4B44287C4D3EDE6D0A",
+	"4B44287C4D3FDE6D0A",
+	"4B44287C4D40DE6D0A",
+	"4B44287C4D41DE6D0A",
+	"4B44287C4D42DE6D0A",
+	"4B44287C4D43DE6D0A",
+	"4B44287C4D44DE6D0A",
+	"4B44287C4D45DE6D0A",
+	"4B44287C4D46DE6D0A",
+	"4B44287C4D47DE6D0A",
+	"4B44287C4D48DE6D0A",
+	"4B44287C4D49DE6D0A",
+	"4B44287C4D4ADE6D0A",
+	"4B44287C4D4BDE6D0A",
+	"4B44287C4D4CDE6D0A",
+	"4B44287C4D4DDE6D0A",
+	"4B44287C4D4EDE6D0A",
+	"4B44287C4D4FDE6D0A",
+}
+
+type Command string
+
+const (
+	R_AD        Command = "41"
+	R_BtnCnfg   Command = "42"
+	R_ChanVrb   Command = "43"
+	R_STATE     Command = "53"
+	R_HIGHLIGHT Command = "48"
+	R_INFO      Command = "49"
+	R_VIDEO     Command = "56"
+	R_KEY       Command = "4B"
+	R_UNIT      Command = "55"
+)
+
+var answers = []string{
+	/*	test_KEY_A,
+		test_KEY_B,
+		test_KEY_C,
+		test_KEY_S,
+		test_KEY_I,
+		test_KEY_H,
+		test_KEY_H1,
+		test_KEY_H2,
+		test_KEY_H3,
+		test_KEY_H4,
+		test_KEY_H5,
+		test_KEY_H6,
+		test_KEY_V, */
+	test_KEY_U,
+	test_KEY_U1,
+	test_KEY_U2,
+}
+
+func GetNextCommand() string {
+	//return "4100112233445566778899AABBCCDDEEFF"
+
+	return answers[rand.Intn(len(answers))]
+	//return test_KEY_K[rand.Intn(len(test_KEY_K))]
+}
+
+func CheckCommand(clickString string) Command {
+	return Command(clickString[0:2])
+}
+
+func main() {
+	rand.Seed(int64(time.Now().Second()))
+
+	for i := 0; i < 5; i++ {
+		clickString := GetNextCommand()
+		fmt.Println("-----------------------------------------------")
+		fmt.Println("Got: ", clickString)
+		switch CheckCommand(clickString) {
+		case R_AD:
+			adEvent := NewAdEvent(clickString)
+			fmt.Printf("Ad event: %s\n", adEvent)
+			fmt.Println("Diagnostics: ", adEvent.BaseEvent.Diagnostic())
+			fmt.Println(adEvent.Command,
+				adEvent.Timestamp,
+				adEvent.AdType,
+				adEvent.AdId,
+				adEvent.Serial,
+				adEvent.Checksum,
+				adEvent.Linefeed)
+		case R_BtnCnfg:
+			btcnfgEvent := NewButtonConfigEvent(clickString)
+			fmt.Printf("Button Config event: %s\n", btcnfgEvent)
+			fmt.Println("Diagnostics: ", btcnfgEvent.BaseEvent.Diagnostic())
+			fmt.Println(btcnfgEvent.Command,
+				btcnfgEvent.Timestamp,
+				btcnfgEvent.ButtonId,
+				btcnfgEvent.ButtonType,
+				btcnfgEvent.ButtonText,
+				btcnfgEvent.ButtonVarData,
+				btcnfgEvent.Serial,
+				btcnfgEvent.Checksum,
+				btcnfgEvent.Linefeed)
+		case R_ChanVrb:
+			channelchange := NewChannelChangeVerboseEvent(clickString)
+			fmt.Printf("Channel change event: %s\n", channelchange)
+			fmt.Println("Diagnostics: ", channelchange.BaseEvent.Diagnostic())
+			fmt.Println(channelchange.Command,
+				channelchange.Timestamp,
+				channelchange.Channel,
+				channelchange.SourseId,
+				channelchange.ProgramId,
+				channelchange.Auth,
+				channelchange.TunerInfo,
+				channelchange.PreviousState,
+				channelchange.LastKey,
+				channelchange.Serial,
+				channelchange.Checksum,
+				channelchange.Linefeed)
+		case R_STATE:
+			statechange := NewStateEvent(clickString)
+			fmt.Printf("State event: %s\n", statechange)
+			fmt.Println("Diagnostics: ", statechange.BaseEvent.Diagnostic())
+			fmt.Println(statechange.Command,
+				statechange.State,
+				statechange.PreviousState,
+				statechange.LastKey)
+		case R_INFO:
+			info := NewInfoScreenEvent(clickString)
+			fmt.Printf("State event: %s\n", info)
+			fmt.Println("Diagnostics: ", info.BaseEvent.Diagnostic())
+			fmt.Println(info.Command,
+				info.Type,
+				info.Id)
+		case R_KEY:
+			key := NewKeyPressEvent(clickString)
+			fmt.Printf("Key Press event: %s\n", key)
+			fmt.Println("Diagnostics: ", key.BaseEvent.Diagnostic())
+			fmt.Println(key.Command,
+				key.KeyCode)
+		case R_HIGHLIGHT:
+			hilit := NewHighlightEvent(clickString)
+			fmt.Printf("Highlight event: %s\n", hilit)
+			fmt.Println("Diagnostics: ", hilit.BaseEvent.Diagnostic())
+			fmt.Println(hilit.Command,
+				hilit.Type,
+				hilit.IdFieldsStr)
+		case R_VIDEO:
+			video := NewVideoPlaybackEvent(clickString)
+			fmt.Printf("Video event: %s\n", video)
+			fmt.Println("Diagnostics: ", video.BaseEvent.Diagnostic())
+			fmt.Println(video.Id,
+				video.VodPlaybackMode,
+				video.Source,
+				video.PlayBackPosition)
+		case R_UNIT:
+			unit := NewUnitIdentificationEvent(clickString)
+			fmt.Printf("Unit Ident event: %s\n", unit)
+			fmt.Println("Diagnostics: ", unit.BaseEvent.Diagnostic())
+			fmt.Println(unit.PeriodicReports,
+				unit.PollingReports,
+				unit.HighWaterMarkReports,
+				unit.BlackoutOverflowReports,
+				unit.ExceededMaxReportsPerHour,
+				unit.UsedBufferSize,
+				unit.GuideState,
+				unit.TunerInfo,
+				unit.SourceIdTuner0,
+				unit.SourceIdTuner1)
+
+		}
+	}
+}
---
+++ commands.go
@@ -0,0 +1,447 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	UTC_GPS_Diff = 315964800
+)
+
+// ---------- Common Base: Command Code, Timestamp, Serial, Checksum, Linefeed --------------------
+type BaseEvent struct {
+	Command   string
+	Timestamp time.Time
+	Serial    string
+	Checksum  string
+	Linefeed  string
+
+	//	Command   [2]string
+	//	Timestamp [8]string
+	//	Serial    [1]string
+	//	Checksum  [1]string
+	//	Linefeed  [1]string
+}
+
+func NewBaseEvent(clickString string) *BaseEvent {
+	bt := new(BaseEvent)
+	// A	time    ... s-n c-s LF
+	// 41 44287C70  ...  B0  E5  0A
+	bt.Command = convertToString(clickString[0:2])
+	bt.Timestamp = convertToTime(clickString[2:10])
+	bt.Serial = clickString[len(clickString)-6 : len(clickString)-4]
+	bt.Checksum = clickString[len(clickString)-4 : len(clickString)-2]
+	bt.Linefeed = clickString[len(clickString)-2 : len(clickString)]
+	return bt
+}
+
+func (bt BaseEvent) String() string {
+	return fmt.Sprintf("Command:[%s]\tTimestamp:[%s]", bt.Command, bt.Timestamp)
+}
+
+func (bt BaseEvent) Diagnostic() string {
+	return fmt.Sprintf("Serial:[%s]\tChecksum:[%s]\tLinefeed:[%s]", bt.Serial, bt.Checksum, bt.Linefeed)
+}
+
+// ---------- Ad: A, 41 --------------------
+type AdEvent struct {
+	*BaseEvent
+	AdType string
+	AdId   string
+	//	AdType    [1]string
+	//	AdId      [4]string
+}
+
+func NewAdEvent(clickString string) *AdEvent {
+	at := new(AdEvent)
+	at.BaseEvent = NewBaseEvent(clickString)
+	// A	time  type  adId   s-n c-s LF
+	// 41 44287C70 00 AB5ADBF2 B0  E5  0A
+	at.AdType = clickString[10:12]
+	at.AdId = clickString[12:20]
+	return at
+}
+
+func (at AdEvent) String() string {
+	return fmt.Sprintf("%s\tAdType:[%s]\tAdId:[%s]", at.BaseEvent, at.AdType, at.AdId)
+}
+
+// ---------- Button Config: B, 42 --------------------
+type ButtonConfigEvent struct {
+	*BaseEvent
+	ButtonId      string
+	ButtonType    string
+	ButtonText    string
+	ButtonVarData string
+}
+
+func NewButtonConfigEvent(clickString string) *ButtonConfigEvent {
+	btcf := new(ButtonConfigEvent)
+
+	btcf.BaseEvent = NewBaseEvent(clickString)
+	//
+	//"42 4427ABE8 00F7 0B 0C 4D6F746F722053706F727473 030B4D6F746F7273706F7274733164646464643164306464306430303164646464646464646464303030303030303130303030303030303030303030303030 60 8C 0A"
+	btcf.ButtonId = clickString[10:14]
+	btcf.ButtonType = clickString[14:16]
+
+	buttonTextLenght := convertToInt(clickString[16:18])
+	btcf.ButtonText = convertToString(clickString[18 : 18+buttonTextLenght*2])
+	btcf.ButtonVarData = clickString[18+buttonTextLenght*2 : len(clickString)-6]
+
+	return btcf
+}
+
+func (btcf ButtonConfigEvent) String() string {
+	return fmt.Sprintf("%s\tButtonId:[%s]\tButtonType:[%s]\tText:[%s]\tData:[%s]",
+		btcf.BaseEvent, btcf.ButtonId, btcf.ButtonType, btcf.ButtonText, btcf.ButtonVarData)
+}
+
+// ---------- Channel Change Verbose: C, 43 --------------------
+
+type ChannelChangeVerboseEvent struct {
+	*BaseEvent
+	Channel       string
+	SourseId      string
+	ProgramId     string
+	Auth          string
+	TunerInfo     string
+	PreviousState string
+	LastKey       string
+}
+
+func NewChannelChangeVerboseEvent(clickString string) *ChannelChangeVerboseEvent {
+	channelchange := new(ChannelChangeVerboseEvent)
+	channelchange.BaseEvent = NewBaseEvent(clickString)
+	// C   time     chN  src prgmId A  TI PS LK
+	// 43 442878E2 01F8 2B57 42AE47 41 00 07 13 AF 3B 0A
+
+	channelchange.Channel = clickString[10:14]
+	channelchange.SourseId = clickString[14:18]
+	channelchange.ProgramId = clickString[18:24]
+	channelchange.Auth = convertToString(clickString[24:26])
+	channelchange.TunerInfo = clickString[26:28]
+	channelchange.PreviousState = clickString[28:30]
+	channelchange.LastKey = clickString[30:32]
+
+	return channelchange
+}
+
+func (channelchange ChannelChangeVerboseEvent) String() string {
+	return fmt.Sprintf("%s\tChannel:[%s]\tSourseId:[%s]\tProgramId:[%s]\tAuth:[%s]\tTuner Info:[%s]\tPrevious State:[%s]\tLast Key:[%s]",
+		channelchange.BaseEvent,
+		channelchange.Channel,
+		channelchange.SourseId,
+		channelchange.ProgramId,
+		channelchange.Auth,
+		channelchange.TunerInfo,
+		channelchange.PreviousState,
+		channelchange.LastKey)
+
+}
+
+// ---------- State: S, 53 --------------------
+
+type StateEvent struct {
+	*BaseEvent
+	State         string
+	PreviousState string
+	LastKey       string
+}
+
+func NewStateEvent(clickString string) *StateEvent {
+	stateEvent := new(StateEvent)
+	stateEvent.BaseEvent = NewBaseEvent(clickString)
+	//  S   time    S PS LK
+	// 53 44287C58 F8 E2 11 EF 93 0A
+	stateEvent.State = clickString[10:12]
+	stateEvent.PreviousState = clickString[12:14]
+	stateEvent.LastKey = clickString[14:16]
+
+	return stateEvent
+}
+
+func (statechange StateEvent) String() string {
+	return fmt.Sprintf("%s\tState:[%s]\tPrevious State:[%s]\tLast Key:[%s]",
+		statechange.BaseEvent,
+		statechange.State,
+		statechange.PreviousState,
+		statechange.LastKey)
+}
+
+// ---------- State: I, 49 --------------------
+
+type InfoScreenEvent struct {
+	*BaseEvent
+	Type string
+	Id   string
+}
+
+func NewInfoScreenEvent(clickString string) *InfoScreenEvent {
+	info := new(InfoScreenEvent)
+	info.BaseEvent = NewBaseEvent(clickString)
+	//
+	// 49 44287C54 56 00EBE822 D5 5B 0A
+	info.Type = convertToString(clickString[10:12])
+	info.Id = clickString[12:20]
+	return info
+}
+
+func (info InfoScreenEvent) String() string {
+	return fmt.Sprintf("%s\tType:[%s]\tId:[%s]",
+		info.BaseEvent,
+		info.Type,
+		info.Id)
+
+}
+
+// ---------- Highlight: H, 48 --------------------
+
+// -----------IdFields struct for Highlight -------
+type IdFields struct {
+	HighlightType string
+	// L or B or G
+	ProgramId string
+	SourceId  string
+	// G
+	GridTime time.Time
+	// M or Q
+	MenuId   string
+	ButtonId string
+	// A
+	Position string
+	// A  or O
+	FunctionCode string
+	// S
+	OptionCode  string
+	OptionValue string
+	// K
+	KeyCode string
+	// D
+	AdId string
+	// V
+	AssetOrSourceId string
+	TemplateId      string
+	ObjectId        string
+	// Common
+	Filler string
+}
+
+func NewIdFields(hiType, clickString string) *IdFields {
+	idFields := new(IdFields)
+	idFields.HighlightType = hiType
+
+	switch idFields.HighlightType {
+	case "L", "B":
+		idFields.ProgramId = clickString[0:6]
+		idFields.SourceId = clickString[6:10]
+		idFields.Filler = clickString[10:18]
+	case "G":
+		idFields.ProgramId = clickString[0:6]
+		idFields.SourceId = clickString[6:10]
+		idFields.GridTime = convertToTime(clickString[10:18])
+	case "M", "Q":
+		idFields.MenuId = clickString[0:4]
+		idFields.ButtonId = clickString[4:8]
+		idFields.Filler = clickString[8:18]
+	case "A":
+		idFields.ProgramId = clickString[0:2]
+		idFields.FunctionCode = clickString[2:6]
+		idFields.Filler = clickString[6:18]
+	case "O":
+		idFields.FunctionCode = clickString[0:4]
+		idFields.Filler = clickString[4:18]
+	case "S":
+		idFields.OptionCode = clickString[0:2]
+		idFields.OptionValue = clickString[2:4]
+		idFields.Filler = clickString[4:18]
+	case "K":
+		idFields.KeyCode = clickString[0:2]
+		idFields.Filler = clickString[2:18]
+	case "D":
+		idFields.AdId = clickString[0:6]
+		idFields.Filler = clickString[6:18]
+	case "V":
+		idFields.AssetOrSourceId = clickString[0:8]
+		idFields.TemplateId = clickString[8:12]
+		idFields.ObjectId = clickString[12:16]
+	}
+	return idFields
+}
+
+func (idFields IdFields) String() string {
+	var str string
+
+	switch idFields.HighlightType {
+	case "L", "B":
+		str = fmt.Sprintf("Program Id: %s\t SourceId %s",
+			idFields.ProgramId,
+			idFields.SourceId)
+	case "G":
+		str = fmt.Sprintf("Program Id: %s\t SourceId %s \t Grid Time: %s ",
+			idFields.ProgramId,
+			idFields.SourceId,
+			idFields.GridTime)
+	case "M", "Q":
+		str = fmt.Sprintf("Menu Id: %s \t Button Id: %s ",
+			idFields.MenuId,
+			idFields.ButtonId)
+	case "A":
+		str = fmt.Sprintf("Program Id: %s\t Function Code: %s",
+			idFields.ProgramId,
+			idFields.FunctionCode)
+	case "O":
+		str = fmt.Sprintf("Function Code: %s",
+			idFields.FunctionCode)
+	case "S":
+		str = fmt.Sprintf("Option Code: %s\t Option Value: %s",
+			idFields.OptionCode,
+			idFields.OptionValue)
+	case "K":
+		str = fmt.Sprintf("Key Code: %s\t",
+			idFields.KeyCode)
+	case "D":
+		str = fmt.Sprintf("Ad Id: %s\t",
+			idFields.AdId)
+	case "V":
+		str = fmt.Sprintf("Asset/Source Id: %s\t Template Id: %s\t Object Id: %s",
+			idFields.AssetOrSourceId,
+			idFields.TemplateId,
+			idFields.ObjectId)
+	}
+	return str
+}
+
+// ---------- Highlight: H, 48 --------------------
+type HighlightEvent struct {
+	*BaseEvent
+	Type        string
+	IdFieldsStr string
+	*IdFields
+}
+
+func NewHighlightEvent(clickString string) *HighlightEvent {
+	hilit := new(HighlightEvent)
+	hilit.BaseEvent = NewBaseEvent(clickString)
+	//
+	// 48 44287C6B 47 486A7926D244286060 FA D5 0A
+	hilit.Type = convertToString(clickString[10:12])
+	hilit.IdFieldsStr = clickString[12:30]
+	hilit.IdFields = NewIdFields(hilit.Type, hilit.IdFieldsStr)
+	return hilit
+}
+
+func (hilit HighlightEvent) String() string {
+	return fmt.Sprintf("%s\tType:[%s]\tIdFileds:[%s]",
+		hilit.BaseEvent,
+		hilit.Type,
+		hilit.IdFields)
+}
+
+// ---------- Video Playback: V, 56 --------------------
+
+type VideoPlaybackEvent struct {
+	*BaseEvent
+	Id               string
+	VodPlaybackMode  string
+	Source           string
+	PlayBackPosition string
+}
+
+func NewVideoPlaybackEvent(clickString string) *VideoPlaybackEvent {
+	video := new(VideoPlaybackEvent)
+	video.BaseEvent = NewBaseEvent(clickString)
+
+	video.Id = clickString[10:24]
+	video.VodPlaybackMode = clickString[24:26]
+	video.Source = convertToString(clickString[26:28])
+	video.PlayBackPosition = clickString[28:32]
+
+	return video
+}
+
+func (video VideoPlaybackEvent) String() string {
+	return fmt.Sprintf("%s\tId:[%s]\tVOD Playback Mode:[%s]\t Source [%s]\t Playback Position: [%s]",
+		video.BaseEvent,
+		video.Id,
+		video.VodPlaybackMode,
+		video.Source,
+		video.PlayBackPosition)
+}
+
+// ---------- Key Press: K, 4B --------------------
+
+type KeyPressEvent struct {
+	*BaseEvent
+	Key     string
+	KeyCode int
+}
+
+func NewKeyPressEvent(clickString string) *KeyPressEvent {
+	key := new(KeyPressEvent)
+	key.BaseEvent = NewBaseEvent(clickString)
+
+	key.KeyCode = int(convertToInt(clickString[10:12]))
+	key.Key = lookUpKeyName(key.KeyCode)
+
+	return key
+}
+
+func (key KeyPressEvent) String() string {
+	return fmt.Sprintf("%s\tKey:[%s]",
+		key.BaseEvent,
+		key.Key)
+}
+
+// ---------- Unit Identification: U, 55 --------------------
+
+type UnitIdentificationEvent struct {
+	*BaseEvent
+	UnitAddress               string
+	HardwareType              string
+	SoftwareVersion           string
+	Profile                   string
+	PeriodicReports           string
+	PollingReports            string
+	HighWaterMarkReports      string
+	BlackoutOverflowReports   string
+	ExceededMaxReportsPerHour string
+	UsedBufferSize            string
+	GuideState                string
+	TunerInfo                 string
+	SourceIdTuner0            string
+	SourceIdTuner1            string
+}
+
+func NewUnitIdentificationEvent(clickString string) *UnitIdentificationEvent {
+	unit := new(UnitIdentificationEvent)
+	unit.BaseEvent = NewBaseEvent(clickString)
+	//																					????
+	// 55 442877A6 00059CAA29 32 33322E343400 0200 00 00 00 00 00 00 00 00 0000 0000 00000000000000000000 00 7F 0A
+	// 55 4428839D 00008B8D72 32 33322E343400 0200 00 00 00 00 00 00 00 00 0000 0000 00000000000000000000 00 66 0A
+	unit.UnitAddress = clickString[10:20]
+	unit.HardwareType = clickString[20:22]
+	unit.SoftwareVersion = convertToString(clickString[22:34])
+	unit.Profile = clickString[34:38]
+	unit.PeriodicReports = clickString[38:40]
+	unit.PollingReports = clickString[40:42]
+	unit.HighWaterMarkReports = clickString[42:44]
+	unit.BlackoutOverflowReports = clickString[44:46]
+	unit.ExceededMaxReportsPerHour = clickString[46:48]
+	unit.UsedBufferSize = clickString[48:50]
+	unit.GuideState = clickString[50:52]
+	unit.TunerInfo = clickString[52:54]
+	unit.SourceIdTuner0 = clickString[54:58]
+	unit.SourceIdTuner1 = clickString[58:62]
+
+	return unit
+}
+
+func (unit UnitIdentificationEvent) String() string {
+	return fmt.Sprintf("%s\tAddr:[%s]\tHardware:[%s]\tSoftware:[%s]\tProfile:[%s]",
+		unit.BaseEvent,
+		unit.UnitAddress,
+		unit.HardwareType,
+		unit.SoftwareVersion,
+		unit.Profile)
+}
